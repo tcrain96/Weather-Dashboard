c0a79423929bd410159c5d7a364b50e61255a686
Tristan Crain <tcrain@metbiblechurch.local>
2021-12-24 11:21:50 -0500

    added basic HTML structure

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//Pseudo Code
+// 1. User either clicks on search form or on a saved city search
+//      a. If user clicks on search form, highlight it and allow user to type city -> Once search is pressed, fetch data on that city and fill other divs
+//              -> Saved searched city in the search history
+//      b. if user clicks on saved city, fetch data for that city
